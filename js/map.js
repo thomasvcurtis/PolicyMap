fd4a164c7bef52dc2ee34439c53cc06379542182
Thomas Curtis II <tcurtis@bgsu.edu>
2023-03-11 15:57:48 -0500

    Restrict map to US

--- js/map.js
+++ js/map.js
@@ -1,5 +1,8 @@
 function makeMap() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiY3JsaWxseSIsImEiOiJjbGY0YnBjbXEwcmd0M3NvNDQ0NzJrY243In0.y62PDs53srP8p8sj90g2XA';
+    //set bound to United States of America
+    const bounds = [21.880392, -160.158985], //Hawaii
+                   [44.780716, -67.078542];  // Maine
     const map = new mapboxgl.Map({
     container: 'map', // container ID
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
