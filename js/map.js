4533511825cadecd14a3a77d936361d2675a93b5
Bryant Whitaker <brywhit@bgsu.edu>
2023-03-11 15:43:36 -0500

    Create map.js and styles.css, modified about_us.html and index.html to use the new files.

---
+++ js/map.js
@@ -0,0 +1,13 @@
+function makeMap() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiY3JsaWxseSIsImEiOiJjbGY0YnBjbXEwcmd0M3NvNDQ0NzJrY243In0.y62PDs53srP8p8sj90g2XA';
+    const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+    });
+
+    const mapDiv = document.getElementById('map');
+    if (mapDiv.style.visibility === true) map.resize();
+}
