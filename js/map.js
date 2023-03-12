dddb97e8f2ddab22682fdad5f60e744670d261e5
Bryant Whitaker <brywhit@bgsu.edu>
2023-03-11 23:16:06 -0500

    Add functionality to map to add pins to the map that are interactable, showing where different petitions cover.

--- js/map.js
+++ js/map.js
@@ -9,7 +9,7 @@ function makeMap() {
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        style: 'mapbox://styles/crlilly/clf4ap806000601qdyny9z6kw', // style URL
         center: [-101.31857987634056, 39.916663896170874], // starting position [lng, lat]
         zoom: 3, // starting zoom
         projection: 'mercator'
@@ -17,4 +17,25 @@ function makeMap() {
     });
 
     map.addControl(new mapboxgl.NavigationControl());
+    
+    map.on('click', (event) => {
+        const features = map.queryRenderedFeatures(event.point, {
+            layers: ['temp-data']
+        });
+        if (!features.length) {
+            return;
+        }
+        const feature = features[0];
+        console.log(feature);
+        document.getElementById("tagElement").click();
+        const title = feature.properties.title;
+        const description = feature.properties.description;
+        const url = feature.properties.URL;
+        document.getElementById("Current_Petition").innerHTML =
+        `<h4>${title}</h4>
+        <p>${description}</p>
+        <p><a href="${url}">Link to the petition</a></p>
+        `;
+    });
 }
+
